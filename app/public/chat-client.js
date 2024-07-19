4e1c988b0645704c6a4bd649fb3c78e0fef08f14
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-07-18 21:01:29 -0400

    Refactor chat client; style ui

--- app/public/chat-client.js
+++ app/public/chat-client.js
@@ -4,39 +4,64 @@ fetch(window.location.origin + '/rooms')
     connect(rooms[0].url);
   });
 
-function connect(url) {
+function connect(url, retryTime = 1000) {
+  const { messageBox, messageInput, messageSubmit } = pageElements();
   const socket = new WebSocket(url);
 
-  const addMessage = (message, { timestamp = true } = {}) => {
+  const addMessage = message => {
     const el = document.createElement('div');
     el.className = 'text-bubble';
-    el.innerText = `${
-      timestamp ? `[${new Date().toLocaleTimeString()}] ` : ''
-    }${message}`;
-    $('messageBox').appendChild(el);
+    el.innerText = `[${new Date().toLocaleTimeString()}] ${message}`;
+    messageBox.appendChild(el);
     el.scrollIntoView({ behavior: 'smooth', block: 'end' });
   };
 
+  // connected to chat server
   socket.onopen = () => {
+    retryTime = 1000; // reset to initial value
     addMessage('Welcome to the chat room!');
   };
 
+  // disconnected from chat server
   socket.onclose = () => {
-    addMessage('Disconnected...');
-    setTimeout(() => connect(url), 1000);
+    if (retryTime === 1000) {
+      addMessage('Disconnected...');
+    } else {
+      addMessage('Attempting to reconnect...');
+    }
+
+    // random time added to stagger attempts if clients disconnected together
+    const exponentialBackoffTime =
+      Math.min(retryTime * 2, 60000) + Math.random() * 900 + 100;
+
+    // attempt to reconnect
+    setTimeout(() => connect(url, exponentialBackoffTime), retryTime);
   };
 
+  // received new message from chat server
   socket.onmessage = event => {
     addMessage(event.data);
   };
 
-  $('messageSendButton').onclick = () => {
-    const message = $('chatInput').value;
-    socket.send(message);
-    $('chatInput').value = '';
+  // send new message to chat server
+  messageSubmit.onclick = () => {
+    socket.send(messageInput.value);
+    messageInput.value = '';
   };
 }
 
-function $(id) {
-  return document.getElementById(id);
+function pageElements() {
+  const $ = id => {
+    const el = document.getElementById(id);
+    if (!el) {
+      throw new Error(`Missing element with ID: '${id}'`);
+    }
+    return el;
+  };
+
+  return {
+    messageBox: $('messageBox'),
+    messageInput: $('messageInput'),
+    messageSubmit: $('messageSubmit'),
+  };
 }
